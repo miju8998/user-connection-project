45c96624bf0e4a0b663c158a64418a5b789e0dbc
Munkhjargal Munkhbat <munkhjargalmunkhbat8998@gmail.com>
2026-03-31 20:48:09 +0200

    Version 3 - updated homepage text

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ session_start();
     <title>User Connection Project</title>
 </head>
 <body>
-    <h1>User Connection Project</h1>
+    <h1>User Connection Project - PHP OOP</h1>
     <a href="register.php">Register</a><br><br>
     <a href="login.php">Login</a>
 </body>
